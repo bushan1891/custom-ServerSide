df75891aa97510f95ee711780153b18af0cf3e26
bushan nagendra <bushan1891@gmail.com>
2016-10-27 21:46:37 +0530

    latest push

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const morgan =  require('morgan');
 const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+
 // initializing express app
 const app = express();
 var Auth0Strategy = require('passport-auth0');
@@ -13,8 +15,8 @@ var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 //DB Setup
  
-//mongoose.connect('localhost:27017/auth');
-mongoose.connect('mongodb://root:root@ds057176.mlab.com:57176/jcstest');
+mongoose.connect('localhost:27017/auth');
+//mongoose.connect('mongodb://root:root@ds057176.mlab.com:57176/jcstest');
 //mongodb://root:root@ds057176.mlab.com:57176/jcstest
 // App set up
 
@@ -22,11 +24,26 @@ mongoose.connect('mongodb://root:root@ds057176.mlab.com:57176/jcstest');
 // morgan is a logging middleware very useful for debugging
 // body parser used for parsing json data
 
+
+
 app.use(morgan('combined'));
+
+app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    }
+);
 app.use(cors());
 app.use(bodyParser.json({type:'*/*'}))
 
 router(app);
+
+//chat 
+
+
 
 
 // server Setup
@@ -36,9 +53,22 @@ const port = process.env.PORT || 5000;
 // forwarding incoming request to express  app
 const server = http.createServer(app);
 server.listen(port);
+var io = require('socket.io')(server);
 console.log('listing on :', port);
 
 
 
 
 
+io.on('connection', function(socket){
+    console.log('connected');
+  socket.emit('an event', { some: 'data' });
+
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log(msg);
+  });
+
+});
